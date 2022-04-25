ca156fca331dcf87aa5a0bc5d308b4abb11a8552
Ted Brookings <tbrookin@broadinstitute.org>
2022-04-25 19:14:17 -0400

    Decreased number of dockers, updated software package versions

---
+++ dockerfiles/sv-base-virtual-env/diff_of_lists.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu -o pipefail
+# given two lists (input as strings of words, or paths to files),
+# output all the words in the first file that are not in the second file
+
+TEMPD=$(mktemp -d)
+trap "rm -rf $TEMPD" EXIT
+
+# print out elements of first list not in second
+{ if [[ -f "$1" ]]; then cat "$1"; else echo "$1"; fi; } | tr -s ' ' '\n' | sort > $TEMPD/list.a
+{ if [[ -f "$2" ]]; then cat "$2"; else echo "$2"; fi; } | tr -s ' ' '\n' | sort > $TEMPD/list.b
+
+comm -23 $TEMPD/list.a $TEMPD/list.b | tr -s '\n' ' '
--- scripts/docker/build_docker.py
+++ scripts/docker/build_docker.py
@@ -109,56 +109,28 @@ class ProjectBuilder:
         "melt": ImageDependencies("dockerfiles/melt/*"),
         "wham": ImageDependencies("dockerfiles/wham/*"),
         "sv-base-mini": ImageDependencies("dockerfiles/sv-base-mini/*"),
-        "samtools-cloud": ImageDependencies("dockerfiles/samtools-cloud/*", {"sv-base-mini": "MINIBASE_IMAGE"}),
+        "samtools-cloud-virtual-env": ImageDependencies("dockerfiles/samtools-cloud-virtual-env/*"),
+        "samtools-cloud": ImageDependencies(
+            "dockerfiles/samtools-cloud/*",
+            {"sv-base-mini": "MINIBASE_IMAGE", "samtools-cloud-virtual-env": "VIRTUAL_ENV_IMAGE"}),
         "sv-base-virtual-env": ImageDependencies("dockerfiles/sv-base-virtual-env/*"),
         "sv-base": ImageDependencies(
             "dockerfiles/sv-base/*",
-            {"sv-base-mini": "MINIBASE_IMAGE", "sv-base-virtual-env": "VIRTUAL_ENV_IMAGE"}),
+            {"samtools-cloud": "SAMTOOLS_CLOUD_IMAGE", "sv-base-virtual-env": "VIRTUAL_ENV_IMAGE"}),
         "cnmops-virtual-env": ImageDependencies(
             "dockerfiles/cnmops-virtual-env/*",
             {"sv-base-virtual-env": "VIRTUAL_ENV_IMAGE"}),
         "cnmops": ImageDependencies(
             ("dockerfiles/cnmops/*", "src/WGD/*"),
             {"sv-base": "SVBASE_IMAGE", "cnmops-virtual-env": "VIRTUAL_ENV_IMAGE"}),
-        "sv-pipeline-base-virtual-env": ImageDependencies(
-            "dockerfiles/sv-pipeline-base-virtual-env/*",
-            {"sv-base-virtual-env": "VIRTUAL_ENV_IMAGE"}),
-        "sv-pipeline-base": ImageDependencies(
-            ("dockerfiles/sv-pipeline-base/*", "src/*"),
-            {"sv-base": "SVBASE_IMAGE", "sv-pipeline-base-virtual-env": "VIRTUAL_ENV_IMAGE"}),
         "sv-pipeline-virtual-env": ImageDependencies(
             "dockerfiles/sv-pipeline-virtual-env/*",
-            {"sv-pipeline-base-virtual-env": "VIRTUAL_ENV_IMAGE"}),
+            {"sv-base-mini": "SV_BASE_MINI_IMAGE",
+             "sv-base-virtual-env": "R_VIRTUAL_ENV_IMAGE",
+             "samtools-cloud-virtual-env": "PYTHON_VIRTUAL_ENV_IMAGE"}),
         "sv-pipeline": ImageDependencies(
-            "dockerfiles/sv-pipeline/*",
-            {"sv-pipeline-base": "SV_PIPELINE_BASE_IMAGE", "sv-pipeline-virtual-env": "VIRTUAL_ENV_IMAGE"}),
-        "sv-pipeline-hail-virtual-env": ImageDependencies(
-            "dockerfiles/sv-pipeline-hail-virtual-env/*",
-            {"sv-pipeline-virtual-env": "VIRTUAL_ENV_IMAGE"}),
-        "sv-pipeline-hail": ImageDependencies(
-            "dockerfiles/sv-pipeline-hail/*",
-            {"sv-pipeline": "SV_PIPELINE_IMAGE", "sv-pipeline-hail-virtual-env": "VIRTUAL_ENV_IMAGE"}),
-        "sv-pipeline-updates-virtual-env": ImageDependencies(
-            "dockerfiles/sv-pipeline-updates-virtual-env/*",
-            {"sv-pipeline-virtual-env": "VIRTUAL_ENV_IMAGE"}),
-        "sv-pipeline-updates": ImageDependencies(
-            "dockerfiles/sv-pipeline-updates/*",
-            {"sv-pipeline": "SV_PIPELINE_IMAGE", "sv-pipeline-updates-virtual-env": "VIRTUAL_ENV_IMAGE"}),
-        "sv-pipeline-children-virtual-env": ImageDependencies(
-            "dockerfiles/sv-pipeline-children-virtual-env/*",
-            {"sv-pipeline-virtual-env": "VIRTUAL_ENV_IMAGE"}),
-        "sv-pipeline-qc-virtual-env": ImageDependencies(
-            "dockerfiles/sv-pipeline-qc-virtual-env/*",
-            {"sv-pipeline-children-virtual-env": "VIRTUAL_ENV_IMAGE"}),
-        "sv-pipeline-qc": ImageDependencies(
-            "dockerfiles/sv-pipeline-qc/*",
-            {"sv-pipeline": "SV_PIPELINE_IMAGE", "sv-pipeline-qc-virtual-env": "VIRTUAL_ENV_IMAGE"}),
-        "sv-pipeline-rdtest-virtual-env": ImageDependencies(
-            "dockerfiles/sv-pipeline-rdtest-virtual-env/*",
-            {"sv-pipeline-children-virtual-env": "VIRTUAL_ENV_IMAGE"}),
-        "sv-pipeline-rdtest": ImageDependencies(
-            "dockerfiles/sv-pipeline-rdtest/*",
-            {"sv-pipeline": "SV_PIPELINE_IMAGE", "sv-pipeline-rdtest-virtual-env": "VIRTUAL_ENV_IMAGE"}),
+            ("dockerfiles/sv-pipeline/*", "src/*"),
+            {"sv-base": "SVBASE_IMAGE", "sv-pipeline-virtual-env": "VIRTUAL_ENV_IMAGE"})
     }
     non_public_images = frozenset({'melt'})
     images_built_by_all = frozenset(dependencies.keys()).difference({"melt"})
